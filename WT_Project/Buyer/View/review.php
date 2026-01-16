f5579550dea4ca6506ae2322e3f668d3b12f290f
Nusrat Mazumder Nisa <nmnisa3102@gmail.com>
2026-01-16 20:59:13 +0600

    Remove 'Review' link from buyer dashboard

--- WT_Project/Buyer/View/review.php
+++ WT_Project/Buyer/View/review.php
@@ -172,7 +172,6 @@ function handleSubmit() {
                 <a class="btn" href="/WT_Project/Buyer/View/buyerDashboard.php">Dashboard</a><br><br>
                 <a class="btn" href="/WT_Project/Buyer/View/searchProduct.php">Search Products</a><br><br>
                 <a class="btn" href="/WT_Project/Buyer/View/placeOrder.php">Place Order</a><br><br>
-                <a class="btn" href="/WT_Project/Buyer/View/review.php">Review</a><br><br>
             <?php endif; ?>
             <a class="btn" href="/WT_Project/User/View/viewProfile.php">View Profile</a><br><br>
             <a class="btn" href="/WT_Project/User/Controller/logout.php">Logout</a>
